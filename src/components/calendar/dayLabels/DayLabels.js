d5e94daa53c11914f69feaeafa83d8e85cafa31f
Rhishabh Hattarki <rhish9h@gmail.com>
2023-08-01 11:41:24 -0700

    basic day labels for calendar

--- src/components/calendar/dayLabels/DayLabels.js
+++ src/components/calendar/dayLabels/DayLabels.js
@@ -1,9 +1,21 @@
 import React from "react";
+import styles from './DayLabels.module.css';
 
 const DayLabels = () => {
+    const days = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
+
     return (
         <div>
-            Day Labels
+            <div className={styles.dayLabels}>
+                {days.map((day, index) => (
+                <div
+                    key={index}
+                    className={styles.dayLabel}
+                >
+                    {day}
+                </div>
+                ))}
+            </div>
         </div>
     )
 }
